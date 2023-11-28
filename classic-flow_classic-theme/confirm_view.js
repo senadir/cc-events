35e22d8f4c64262e997a014228510e0c9590827d
Nadir Seghir <nadir.seghir@gmail.com>
2023-11-28 18:09:06 +0100

    init

---
+++ classic-flow_classic-theme/confirm_view.js
@@ -0,0 +1,26 @@
+const woocommerceanalytics_order_confirmation_view = {
+  cart_page_contains_cart_block: 0,
+  cart_page_contains_cart_shortcode: 1,
+  checkout_page_contains_checkout_block: 0,
+  checkout_page_contains_checkout_shortcode: 1,
+  create_account: No,
+  device: desktop,
+  guest_checkout: Yes,
+  lr: null,
+  or: null,
+  products: {
+    34: {
+      pi: 14,
+      pq: 1,
+      pt: simple,
+      pn: "Woo Test",
+      pc: "",
+      pp: "10",
+    },
+  },
+  r3d: null,
+  store_currency: EUR,
+  ui: null,
+  url: "https://wca.jurassic.tube",
+  woo_version: "8.3.1",
+};
